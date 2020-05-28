52779c5f1cf9ea087f4ac148c3186da0d59df846
Miguel Lorenzo V Dungan <miguel_dungan@dlsu.edu.ph>
2020-05-28 11:24:17 +0800

    returning control so number

--- app-ui/src/app/admin/sales/sales-orders/sales-order-form/sales-order-form.controller.js
+++ app-ui/src/app/admin/sales/sales-orders/sales-order-form/sales-order-form.controller.js
@@ -1,30 +1,30 @@
 
 function SalesOrderFormController($state, SalesOrdersService, ItemsService, ProductInventoryService, PurchaseOrdersService, UsersService, $rootScope) {
   var ctrl = this;
-  
+
   ctrl.stockOnHandList = [];
-  ctrl.reserved= [];
-  ctrl.$onInit = function(){
-	  ctrl.user = JSON.parse(window.localStorage.getItem("currentUser"));
-	  
-	  UsersService.get(ctrl.user.id).then(function(response){
-		  ctrl.depots = response.data.depots;
-	  });
-	  
-	  ctrl.so.date = new Date();
+  ctrl.reserved = [];
+  ctrl.$onInit = function () {
+    ctrl.user = JSON.parse(window.localStorage.getItem("currentUser"));
+    ctrl.so.number = 0001;
+    UsersService.get(ctrl.user.id).then(function (response) {
+      ctrl.depots = response.data.depots;
+    });
+
+    ctrl.so.date = new Date();
   };
   ctrl.$onChanges = function (changes) {
     if (changes.so) {
       ctrl.so = angular.copy(ctrl.so);
     }
   };
-  
-  
+
+
   ctrl.submitForm = function () {
     console.log('submitForm: POTANGINA ' + JSON.stringify(ctrl.so));
-    for(var i = 0; i < ctrl.so.products.length; i++){
-    	ctrl.so.products[i].depot = ctrl.so.depot;
-    	ctrl.so.products[i].soNumber = ctrl.so.number;
+    for (var i = 0; i < ctrl.so.products.length; i++) {
+      ctrl.so.products[i].depot = ctrl.so.depot;
+      ctrl.so.products[i].soNumber = ctrl.so.number;
     }
     console.log('HERE')
     SalesOrdersService.save(ctrl.so)
@@ -33,29 +33,29 @@ function SalesOrderFormController($state, SalesOrdersService, ItemsService, Prod
         so: ctrl.so
       }
     });
-  
+
   };
-  
-  
-  
-  ctrl.computeTotalAmount = function(value, index){
-	  ctrl.so.products[index].amount = value;
-	  ctrl.so.totalAmount = 0;
-	  for(var i = 0; i < ctrl.so.products.length; i++){
-		  ctrl.so.totalAmount += ctrl.so.products[i].quantity * ctrl.so.products[i].unitPrice ;
-	  }
+
+
+
+  ctrl.computeTotalAmount = function (value, index) {
+    ctrl.so.products[index].amount = value;
+    ctrl.so.totalAmount = 0;
+    for (var i = 0; i < ctrl.so.products.length; i++) {
+      ctrl.so.totalAmount += ctrl.so.products[i].quantity * ctrl.so.products[i].unitPrice;
+    }
   };
-  
-  ctrl.findProduct = function(){
-	  ctrl.company = $rootScope.selectedCompany;
-	  ProductInventoryService.listFinishedGoodView(ctrl.company.id).then(function(response){
-		  ctrl.fglistview = response.data;
-		  console.log("FGLISTVIEW" + ctrl.fglistview);
-		  $("#findFgInventoryModal").modal('show');
-	  });
-		 
+
+  ctrl.findProduct = function () {
+    ctrl.company = $rootScope.selectedCompany;
+    ProductInventoryService.listFinishedGoodView(ctrl.company.id).then(function (response) {
+      ctrl.fglistview = response.data;
+      console.log("FGLISTVIEW" + ctrl.fglistview);
+      $("#findFgInventoryModal").modal('show');
+    });
+
   };
-  
+
 
 
 }
