ca64c7728370827e23936fd0c97af9de5be263a7
Raymund Micoh Alvarez <micohalvarez@yahoo.com>
2020-06-08 07:46:54 +0800

    fixed  classifications bugs

--- app-ui/src/app/admin/maintenance/classifications/classifications/classifications.controller.js
+++ app-ui/src/app/admin/maintenance/classifications/classifications/classifications.controller.js
@@ -1,5 +1,9 @@
-
-function ClassificationController($state, ClassificationsService, $rootScope, _) {
+function ClassificationController(
+  $state,
+  ClassificationsService,
+  $rootScope,
+  _
+) {
   var ctrl = this;
   ctrl.classifications = [];
   ctrl.companies = [];
@@ -8,42 +12,81 @@ function ClassificationController($state, ClassificationsService, $rootScope, _)
   ctrl.searchName = '';
 
   ctrl.sortReverse = false;
-  
+
   ctrl.$onInit = function () {
-	  ctrl.addClassification = false;
-	  ctrl.error = null;
-	  loadClassifications();
+    ctrl.addClassification = false;
+    ctrl.error = null;
+    loadClassifications();
   };
-  
-  function loadClassifications(){
-	  ClassificationsService.list().then(function(response){
-		  console.log("list response: " + JSON.stringify(response.data));
-		  ctrl.classifications = response.data;
-	  });
+
+  function loadClassifications() {
+    ClassificationsService.list().then(function (response) {
+      console.log('list response: ' + JSON.stringify(response.data));
+      ctrl.classifications = response.data;
+    });
   }
-  
-  ctrl.showAddClassification = function (show){
-	  ctrl.addClassification = show;
+
+  ctrl.showAddClassification = function (show) {
+    ctrl.addClassification = show;
   };
-  
+
+  ctrl.confirmDelete = function (id, name) {
+    $('#confirmDeleteAction').modal('show');
+    ctrl.itemName = 'Are you sure you want to delete ' + name + '?';
+    ctrl.id = id;
+  };
+
+  ctrl.confirmEdit = function (event) {
+    $('#confirmEditAction').modal('show');
+    ctrl.itemName = 'Are you sure you want to save changes ?';
+    ctrl.event = event;
+  };
+
+  ctrl.cancelDelete = function () {
+    $('#confirmDeleteAction').modal('hide');
+  };
+
+  ctrl.cancelEdit = function () {
+    $('#confirmEditAction').modal('hide');
+  };
+
+  ctrl.confirm = function () {
+    $('#actionDialog').modal('hide');
+  };
+
   ctrl.editClassification = function (id) {
-	  ClassificationsService.get(id).then(function(response){
-		  ctrl.classification = response.data;
-	  });
-	  ctrl.addClassification = true;
+    ClassificationsService.get(id).then(function (response) {
+      ctrl.classification = response.data;
+    });
+    ctrl.addClassification = true;
   };
-  
+
   ctrl.saveClassification = function (event) {
-	    ClassificationsService.save(event.classification).then(function () {
-	    	  loadClassifications();
-	    	  ctrl.showAddClassification(false);
-	    });
+    $('#confirmEditAction').modal('hide');
+    ClassificationsService.save(event.classification).then(function () {
+      ctrl.actionTitle = 'Confirmation';
+      ctrl.actionMessage = 'You have successfully updated the list';
+      loadClassifications();
+      ctrl.showAddClassification(false);
+    });
+
+    $('#actionDialog').modal('show');
   };
-  
-  ctrl.deleteClassification = function (id){
-	  ClassificationsService.delete(id).then(function(response){
-		  loadClassifications();
-	  });
+
+  ctrl.deleteClassification = function (id) {
+    $('#confirmDeleteAction').modal('hide');
+    ClassificationsService.delete(id)
+      .then(function (response) {
+        loadClassifications();
+        ctrl.actionTitle = 'Confirmation';
+        ctrl.actionMessage = 'You have successfully deleted the item';
+      })
+      .catch(function (error) {
+        ctrl.actionTitle = 'Error';
+        ctrl.actionMessage = 'The item was not deleted';
+      });
+    $('#confirmAction').modal('hide');
+    $('#actionDialog').modal('show');
   };
 }
 
