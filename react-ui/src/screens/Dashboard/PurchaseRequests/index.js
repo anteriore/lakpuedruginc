2e2d11fa29dc3fa6b6f1ff7c14be24f39c2cc341
Angelo Matias <geloko@users.noreply.github.com>
2020-11-05 18:52:50 +0800

    Pagination

--- react-ui/src/screens/Dashboard/PurchaseRequests/index.js
+++ react-ui/src/screens/Dashboard/PurchaseRequests/index.js
@@ -3,29 +3,22 @@ import { Row, Col, Tabs, Table, Typography, Button, Modal, Skeleton, Empty, Popc
 import { 
     EditOutlined,
     DeleteOutlined,
-    PlusOutlined
+    PlusOutlined,
+    QuestionCircleOutlined
 } from '@ant-design/icons';
 import { Switch, Route, useRouteMatch, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
 
 import { getPR, listPR, deletePR, resetItemData } from './redux'
 import InputForm from './InputForm'
-import moment from 'moment';
+import TableDisplay from '../../../components/TableDisplay'
 
 const { Title } = Typography;
 
 const PurchaseRequests = (props) => {
     const [loading, setLoading] = useState(false)
     const [loadingItem, setLoadingItem] = useState(false)
-    const [defaultpageSize, setDefaultPageSize] = useState(6)
-    const [pageSize, setPageSize] = useState(6)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [dataCount, setDataCount] = useState(0)
-    const [range, setRange] = useState([1,6])
-    const [offset, setOffset] = useState(0)
-    const [sorter, setSorter] = useState(null)
-    const [filters, setFilters] = useState(null)
-    const [searchText, setSearchText] = useState(null)
 
     const [displayModal, setDisplayModal] = useState(false);
     const [displayData, setDisplayData] = useState(null);
@@ -34,30 +27,40 @@ const PurchaseRequests = (props) => {
         {
             title: 'PRF Number',
             dataIndex: 'number',
-            key: 'number',   
+            key: 'number',
+            defaultSortOrder: 'ascend',
+            sorter: (a, b) => a.number - b.number 
         },
         {
             title: 'PRF Date',
             dataIndex: 'date',
             key: 'date',
-            render: (text) => moment(new Date(text)).format("DD/MM/YYYY")   
+            render: (text) => moment(new Date(text)).format("DD/MM/YYYY"),
+            defaultSortOrder: 'ascend',
+            sorter: (a, b) => a.date - b.date    
         },
         {
             title: 'Date Needed',
             dataIndex: 'dateNeeded',
             key: 'dateNeeded',
-            render: (text) => moment(new Date(text)).format("DD/MM/YYYY")    
+            render: (text) => moment(new Date(text)).format("DD/MM/YYYY"),
+            defaultSortOrder: 'ascend',
+            sorter: (a, b) => a.dateNeeded - b.dateNeeded       
         },
         {
             title: 'Department',
             dataIndex: 'department',
             key: 'department',   
-            render: (object) => object != null ? (object.name) : ('')
+            render: (object) => object != null ? (object.name) : (''),
+            defaultSortOrder: 'ascend',
+            sorter: (a, b) => a.department - b.department    
         },
         {
             title: 'Status',
             dataIndex: 'status',
-            key: 'status',   
+            key: 'status',
+            defaultSortOrder: 'ascend',
+            sorter: (a, b) => a.status - b.status       
         }
     ])
 
@@ -137,7 +140,8 @@ const PurchaseRequests = (props) => {
                                 Edit
                             </Button>
                             <Popconfirm
-                                title="Are you sure delete this item?"
+                                title="Would you like to delete this item?"
+                                icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
                                 onConfirm={(e) => {
                                     e.stopPropagation() 
                                     dispatch(deletePR(row.id))
@@ -174,82 +178,31 @@ const PurchaseRequests = (props) => {
         return(filteredColumn)
     }
 
-    const onChangePage = (page, pageSize) => {
-        if(loading == false){
-            setCurrentPage(page)
-        }    
-    }
-
-    const onChangePageSize = (currentPage, newPageSize) => {
-        if(pageSize !== newPageSize && loading == false){
-            setPageSize(newPageSize)
-            setCurrentPage(1)
-            setOffset(0)               
-            getTableData()
-        }   
-    }
-
-    const onChangeRange = (total, newRange) => {  
-
-        if(range[0] !== newRange[0] || range[1] !== newRange[1] ){
-            var offset = newRange[0]-1        
-            setRange(newRange)                      
-            getTableData()
-        }
-    }
-
-    const getTableData = () => {
-        /*
-        setLoading(true)
-        var controllerText = `?limit=${pageSize}&offset=${offset}`
-
-        if(searchText !== null){         
-            controllerText = controllerText + `&search=${searchText}`
-        }
-
-        if(filters !== null){
-            controllerText = controllerText + filters
-        }
-
-        if(sorter !== null){
-            controllerText = controllerText + sorter
-        }     
-        
-        props.getData(API_URL, controllerText) 
-        */    
-
-    }
-
-    const handleTableChange = (pagination, filters, newSorter) => {
-        handleSorter(newSorter)     
-    };
-  
-    const handleSorter = (newSorter) => {
-  
-        if(sorter !== newSorter && sorter !== null){
-          if(newSorter.order === 'ascend'){
-              setSorter(`&ordering=${newSorter.columnKey}`)
-              getTableData()
-          }else if(newSorter.order === 'descend'){
-              setSorter(`&ordering=-${newSorter.columnKey}`)
-              getTableData()
-          }
-          else if(sorter.order === undefined){
-              setSorter(null)
-          }      
-        }
-  
-    }
-
     const viewPurchaseRequest = (key) => {
-        setLoadingItem(true)
-        dispatch(getPR({id: key}))
-        setDisplayModal(true)
     }
 
     const closeModal = () => {
         setDisplayModal(false)
         setDisplayData(null)
+    }
+
+    const handleUpdate = (data) => {
+        history.push(path + "/" + data.id)
+
+    }
+
+    const handleDelete = (data) => {
+        dispatch(deletePR(data.id))
+            .then((response) => {
+                dispatch(listPR({company: company}))
+                message.success("Successfully deleted Purchase Request " + data.number)
+            })
+
+    }
+    const handleRetrieve = (data) => {
+        setLoadingItem(true)
+        dispatch(getPR({id: data.id}))
+        setDisplayModal(true)
     }
 
     return (
@@ -277,28 +230,12 @@ const PurchaseRequests = (props) => {
                     </Row>
                     <Row>
                         <Col span={20}>
-                        <Table
-                            loading={loading}
-                            dataSource={data}
-                            columns={columnfilter()} 
-                            sorter={true}
-                            pagination={ {
-                                onChange: (page, pageSize) => { onChangePage(page, pageSize) },
-                                showTotal: (total, range)	=> { onChangeRange(total, range) },   
-                                onShowSizeChange:(current, size)=>{ onChangePageSize(current, size) },  
-                                current: currentPage,                               
-                                showQuickJumper:true,                              
-                                defaultPageSize: defaultpageSize,
-                                pageSizeOptions:[defaultpageSize, '20', '50', '100'],
-                                showSizeChanger:true,
-                                total:dataCount }
-                            }
-                            onRow={(record, rowIndex) => {
-                                    return {                             
-                                        onClick: () => {viewPurchaseRequest(record.id)}, // click row
-                                    };
-                            }}
-                            onChange={handleTableChange}
+                        <TableDisplay
+                            columns={columns} 
+                            data={data} 
+                            handleRetrieve={handleRetrieve}
+                            handleUpdate={handleUpdate}
+                            handleDelete={handleDelete}
                         />
                         </Col>
                     </Row>
