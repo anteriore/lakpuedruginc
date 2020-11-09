bddb8708ee79e841424877a249c94fb5582545fe
ESP12 <helias@DESKTOP-POGPB3M.localdomain>
2020-11-09 20:11:54 +0800

    Refactor table and forms on finished goods module and units

--- react-ui/src/screens/Maintenance/FinishedGoods/index.js
+++ react-ui/src/screens/Maintenance/FinishedGoods/index.js
@@ -4,23 +4,18 @@ import {
   Col, 
   Typography, 
   Button,
-  Table,
-  Modal,
   message
 } from 'antd';
 import { 
   PlusOutlined,
-  ExclamationCircleOutlined
 } from '@ant-design/icons';
-import { tableHeader } from '../../../datas/FinishedGoods'
-import FilteredColumns from '../../../components/TableDisplay/FilteredColumns';
-import TableSearch from '../../../components/TableDisplay/TableSearch';
-import FinishedGoodsForm from '../../../components/forms/FinishedGoodsForm';
+import { tableHeader, formDetails } from './data';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFGList, createFG, deleteFG, updateFG } from './redux';
+import TableDisplay from '../../../components/TableDisplay';
+import SimpleForm from '../../../components/forms/SimpleForm';
 
 const { Title } = Typography;
-const { confirm } = Modal;
 
 const FinishedGoods = (props) => {
   const { company } = props;
@@ -61,21 +56,11 @@ const FinishedGoods = (props) => {
   }
 
   const handleDeleteButton = (row) => {
-    confirm({
-      title: 'Do you want to delete this item?',
-      icon: <ExclamationCircleOutlined />,
-      content: `Item with id of (${row.id}) and name of (${row.name})  will be deleted.`,
-      onOk() {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            dispatch(deleteFG(row)).then(() => {
-              dispatch(getFGList());
-              resolve();
-            });
-          }, 1000);
-        }).catch(() => message.error("Oops! something went wrong."));
-      },
-      onCancel() {},
+    dispatch(deleteFG(row)).then(() => {
+      message.success(statusMessage);
+      dispatch(getFGList());
+    }).catch((err) => {
+      message.error(`Something went wrong! Error: ${err}`);
     });
   }
 
@@ -116,17 +101,19 @@ const FinishedGoods = (props) => {
         </Button>
       </Col>
       <Col span={20}>
-        <Table
-          dataSource={list}
-          columns={FilteredColumns(TableSearch(tableHeader), handleEditButton, handleDeleteButton)}
-        />
+        <TableDisplay 
+          columns={tableHeader}
+          data={list}
+          handleUpdate={handleEditButton}
+          handleDelete={handleDeleteButton} />
       </Col>
-      <FinishedGoodsForm 
-        visible={isOpenForm} 
-        title={modalTitle} 
-        onSubmit={onSubmit} 
-        values={formValues} 
-        onCancel={handleCancelButton} 
+      <SimpleForm
+        visible={isOpenForm}
+        title={modalTitle}
+        onSubmit={onSubmit}
+        values={formValues}
+        onCancel={handleCancelButton}
+        formDetails={formDetails}
       />
     </Row>
   )
