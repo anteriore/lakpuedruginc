eb836d42149692cc075399147301304c5f31af22
Angelo Matias <geloko@users.noreply.github.com>
2020-11-11 17:42:51 +0800

    Redirect from root url

--- react-ui/src/navigation/routes/AdminRoutes.js
+++ react-ui/src/navigation/routes/AdminRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 
 import Dashboard from '../../screens/Dashboard';
 import Maintenance from '../../screens/Maintenance';
@@ -12,24 +12,26 @@ import Maintenance from '../../screens/Maintenance';
 // import Costing from '../../screens/Costing/';
 
 const AdminRoutes = () => {
-  return (
-    <div>
-      <Switch>
-        <Route path="/maintenance" component={Maintenance} />
-        {/* <Route path="/users" component={Users} /> */}
-        {/* <Route path="/accounting" component={Accounting} /> */}
-        {/* <Route path="/sales" component={Sales} /> */}
-        {/* <Route path="/mmd" component={MMD} /> */}
-        {/* <Route path="/rnd" component={RND} /> */}
-        {/* <Route path="/purchasing" component={Purchasing} /> */}
-        {/* <Route path="/costing" component={Costing} /> */}
-        <Route path="/dashboard" component={Dashboard} />
-        <Route exact path="/">
-          <Redirect to="/dashboard" />
-        </Route>
-      </Switch>
-    </div>
-  );
+    const history = useHistory();
+    return (
+        <div>
+        <Switch>
+            <Route path="/maintenance" component={Maintenance} />
+            {/* <Route path="/users" component={Users} /> */}
+            {/* <Route path="/accounting" component={Accounting} /> */}
+            {/* <Route path="/sales" component={Sales} /> */}
+            {/* <Route path="/mmd" component={MMD} /> */}
+            {/* <Route path="/rnd" component={RND} /> */}
+            {/* <Route path="/purchasing" component={Purchasing} /> */}
+            {/* <Route path="/costing" component={Costing} /> */}
+            <Route path="/dashboard" component={Dashboard} />
+            <Route exact path="/" render={() => {
+                history.push("/dashboard")
+            }}>
+            </Route>
+        </Switch>
+        </div>
+    );
 };
 
 export default AdminRoutes;
