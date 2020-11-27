901f49c72295574b69e171ce82d95bcc4214418f
ESP12 <helias@DESKTOP-POGPB3M.localdomain>
2020-11-27 17:47:32 +0800

    Adjust input table layout

--- react-ui/src/screens/Sales/SalesOrders/InputForm.js
+++ react-ui/src/screens/Sales/SalesOrders/InputForm.js
@@ -5,18 +5,22 @@ import {
   Typography,
   Form,
   Table,
+  Space,
+  Button
 } from 'antd';
 import Layout from 'antd/lib/layout/layout';
 import { useForm } from 'antd/lib/form/Form';
 import { formDetails, tableProduct } from './data';
 import FormItem from '../../../components/forms/FormItem';
 import _ from 'lodash';
+import { useHistory } from 'react-router-dom';
 
 const {Title} = Typography;
 
 const InputForm = (props) => {
   const { title } = props;
   const [form] = useForm();
+  const history = useHistory();
 
   return (
     <>
@@ -31,13 +35,23 @@ const InputForm = (props) => {
             {...styles.formLayout}
           >
             {_.dropRight(formDetails.form_items).map((item) => <FormItem item={item}/>)}
-            <Form.Item wrapperCol={{span: 18, offset: 3}}>
+            <Form.Item wrapperCol={{span: 15, offset: 4}}>
               <Table 
                 columns={tableProduct}
                 dataSource={null}
               />
             </Form.Item>
             <FormItem item={_.last(formDetails.form_items)} />
+            <Form.Item wrapperCol={{ offset: 15, span: 4 }}>
+              <Space size={16}>
+                <Button htmlType="button" onClick={() => history.goBack()}>
+                  Cancel
+                </Button>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Space>
+            </Form.Item>
           </Form>
           </Layout>
         </Col>
