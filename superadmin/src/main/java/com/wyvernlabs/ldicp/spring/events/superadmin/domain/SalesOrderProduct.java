ff28ffd58395fcae358905a1ab36ff754cc61e92
Miguel Lorenzo V Dungan <miguel_dungan@dlsu.edu.ph>
2020-12-15 16:30:23 +0800

    product update

--- superadmin/src/main/java/com/wyvernlabs/ldicp/spring/events/superadmin/domain/SalesOrderProduct.java
+++ superadmin/src/main/java/com/wyvernlabs/ldicp/spring/events/superadmin/domain/SalesOrderProduct.java
@@ -18,6 +18,10 @@ public class SalesOrderProduct {
 	private Long id;
 	@OneToOne
 	private FinishedGood finishedGood;
+	//added
+	@OneToOne
+	private Product product;
+
 	private int quantity;
 	private String status = "Pending";
 	private String soNumber;
@@ -35,6 +39,14 @@ public class SalesOrderProduct {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Product getProduct(){
+		return product;
+	}
+
+	public void setProduct(Product product){
+		this.product = product;
 	}
 
 	public FinishedGood getFinishedGood() {
