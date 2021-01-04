8fef731513a472b57927b14801faa72e20792521
ESP12 <helias@DESKTOP-POGPB3M.localdomain>
2021-01-04 19:41:40 +0800

    added full dom testing

--- react-ui/src/tests/components/sideNav.test.js
+++ react-ui/src/tests/components/sideNav.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import SideNav from '../../components/SideNav/';
+import {BrowserRouter ,Switch, Route} from'react-router-dom';
+import sinon from 'sinon';
 
 const listSideNav = [
   "Dashboard",
@@ -14,12 +16,12 @@ const listSideNav = [
   "Costing"
 ]
 
-describe('SideNav', () => {
+describe('SideNav shallow test', () => {
   const wrapper = shallow(<SideNav location={{
     pathname: "maintenance"
   }} />);
 
-  it('check menu items not equal to 0', () => {
+  it('checks menu items not equal to 0', () => {
     expect(wrapper.children().length).toBeGreaterThan(0)
   })
 
@@ -36,3 +38,27 @@ describe('SideNav', () => {
     })
   })
 })
+
+describe('SideNav full dom test', () => {
+  const wrapper = mount(<SideNav location={{pathname: "maintenance"}} />, {wrappingComponent: BrowserRouter})
+  it('checks successful render inside BrowserRouter', () => {
+    let finalResults = [];
+
+    listSideNav.forEach((nav,i) => {
+      const results = wrapper.childAt(0).find('a').at(i).contains(nav);
+      finalResults.push(results);
+    })
+    
+    finalResults.forEach(val => {
+      expect(val).toBe(true);
+    });
+  });
+
+  it('check has right props', () => {
+    const menuItems = wrapper.childAt(0).find('MenuItem');
+    menuItems.forEach(item => {
+      expect(item.find('LinkAnchor').prop('href')).not.toBe('')
+      expect(item.find('LinkAnchor').prop('href')).not.toBe(null)
+    })
+  })
+});
