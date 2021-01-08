3cc61508ec5ee3af448518c6cd4652fb75db144d
Miguel Lorenzo V Dungan <miguel_dungan@dlsu.edu.ph>
2021-01-08 14:53:30 +0800

    data stabilize

--- superadmin/src/main/java/com/wyvernlabs/ldicp/spring/events/superadmin/data/ProductData.java
+++ superadmin/src/main/java/com/wyvernlabs/ldicp/spring/events/superadmin/data/ProductData.java
@@ -19,6 +19,13 @@ import com.wyvernlabs.ldicp.spring.events.superadmin.repository.ProductDivisionR
 import com.wyvernlabs.ldicp.spring.events.superadmin.repository.ProductRepository;
 import com.wyvernlabs.ldicp.spring.events.superadmin.repository.UnitRepository;
 
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
 @Component
 public class ProductData {
 	@Autowired
@@ -42,7 +49,7 @@ public class ProductData {
 		Company company = companyRepository.getOne(1L);
 		Classification classification = classificationRepository.getOne(1L);
 
-		product.setBigUnit(unitRepository.findByCode("g"));
+		
 		product.setClassification(classification);
 		product.setDivision(divisionRepository.getOne(1L));
 		product.setCategory(categoryRepository.getOne(1L));
@@ -51,6 +58,7 @@ public class ProductData {
 		product.setLotNumber("LOT#12345");
 		product.setQuantityPerBox(25);
 		product.setReorderLevel(1000);
+		product.setBigUnit(unitRepository.findByCode("g"));
 		product.setSmallUnit(unitRepository.findByCode("kg"));
 		product.setUnitPrice(50D);
 		product.setFinishedGood(fg);
@@ -60,17 +68,85 @@ public class ProductData {
 
 
 		
-		//BUOM
+		//finishedgood
 		//(no classification)
 		//USID DIVISION ID
 		//(no category)
+		//setpackagedescription
 		//company number
 		//expiration (fit expire(but its like just one integer))
 		//FIT_LOTNO-lot number
 		//FIT_QPB-quantity per box
 		//OSCH-reorder level
+		//BUOM
 		//SUOM
 		//FITUNITP-unitprice
 		//FITCODE-finished good
+		//readCSV("groupData.csv");
 	}
+
+
+
+
+
+
+
+
+	public void readCSV(String csvname){
+		String csvFile = "../src/main/java/com/wyvernlabs/ldicp/spring/events/superadmin/csv/"+csvname;
+        BufferedReader br = null;
+        String line = "";
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+		Company company = companyRepository.getOne(1L);
+        try {				
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+				
+
+					Product tempproduct = new Product();
+					//tempproduct.setName(data[0].replace("\"", ""));
+					//tempproduct.setName(data[1].replace("\"", ""));
+					tempproduct.setCompany(company);
+					productRepository.save(tempproduct);
+
+
+              
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+	
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
