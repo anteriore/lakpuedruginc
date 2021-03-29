9314bc793f37a20ef27dd81d01aee33e37c6c68a
THEREALHELIAS <helias@DESKTOP-POGPB3M.localdomain>
2021-03-29 13:55:06 +0800

    Added loading screens in maintenance-units

--- react-ui/src/screens/Maintenance/Units/index.js
+++ react-ui/src/screens/Maintenance/Units/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Typography, Col, Button, message } from 'antd';
+import { Row, Typography, Col, Button, message, Skeleton } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import GeneralStyles from '../../../data/styles/styles.general';
 import { tableHeader, formDetails } from './data';
@@ -17,12 +17,14 @@ const Units = (props) => {
   const [mode, setMode] = useState('');
   const [formValues, setFormValues] = useState('');
   const [currentID, setCurrentID] = useState('');
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const { unitList, action, statusMessage } = useSelector((state) => state.maintenance.units);
 
   useEffect(() => {
     let isCancelled = false;
     dispatch(listUnit({ company, message })).then(() => {
+      setLoading(false);
       if (isCancelled) {
         dispatch(clearData());
       }
@@ -61,9 +63,12 @@ const Units = (props) => {
   };
 
   const handleDeleteButton = (row) => {
+    setLoading(true);
     dispatch(deleteUnit(row))
       .then(() => {
-        dispatch(listUnit({ company, message }));
+        dispatch(listUnit({ company, message })).then(() => {
+          setLoading(false);
+        });
       })
       .catch((err) => {
         message.error(`Something went wrong! details: ${err}`);
@@ -76,16 +81,21 @@ const Units = (props) => {
   };
 
   const onSubmit = (values) => {
+    setLoading(true);
     if (mode === 'edit') {
       const newValues = values;
       newValues.id = currentID;
 
       dispatch(updateUnit(newValues)).then(() => {
-        dispatch(listUnit({ message }));
+        dispatch(listUnit({ message })).then(() => {
+          setLoading(false)
+        });
       });
     } else if (mode === 'add') {
       dispatch(createUnit(values)).then(() => {
-        dispatch(listUnit({ message }));
+        dispatch(listUnit({ message })).then(() => {
+          setLoading(false)
+        });;
       });
     }
     setFormValues('');
@@ -97,20 +107,22 @@ const Units = (props) => {
       <Col style={GeneralStyles.headerPage} span={20}>
         <Title>{title}</Title>
         {actions.includes('create') && (
-          <Button icon={<PlusOutlined />} onClick={() => handleAddButton()}>
+          <Button icon={<PlusOutlined />} loading={loading} onClick={() => handleAddButton()}>
             Add
           </Button>
         )}
       </Col>
       <Col span={20}>
-        <TableDisplay
-          columns={tableHeader}
-          data={unitList}
-          handleUpdate={handleEditButton}
-          handleDelete={handleDeleteButton}
-          updateEnabled={actions.includes('update')}
-          deleteEnabled={actions.includes('delete')}
-        />
+        { loading ? <Skeleton/> : 
+          <TableDisplay
+            columns={tableHeader}
+            data={unitList}
+            handleUpdate={handleEditButton}
+            handleDelete={handleDeleteButton}
+            updateEnabled={actions.includes('update')}
+            deleteEnabled={actions.includes('delete')}
+          />
+        }
       </Col>
       <SimpleForm
         visible={isOpenForm}
