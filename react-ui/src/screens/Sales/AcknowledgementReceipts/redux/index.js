c2e4d68004ecb3c75ab82a5be870a92cd1ba24c2
THEREALHELIAS <helias@DESKTOP-POGPB3M.localdomain>
2021-04-13 00:47:04 +0800

    Remove unused variables Added initial state value on redux reducer of AR module Added prompt messages Added performCleanUp function Added prompt messages for dependency datas Refactor onSubmit

--- react-ui/src/screens/Sales/AcknowledgementReceipts/redux/index.js
+++ react-ui/src/screens/Sales/AcknowledgementReceipts/redux/index.js
@@ -2,64 +2,56 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 import axiosInstance from '../../../../utils/axios-instance';
 import * as message from '../../../../data/constants/response-message.constant';
+import { checkResponseValidity, generateStatusMessage } from '../../../../helpers/general-helper';
 
 const initialState = {
-  list: null,
-  status: '',
+  list: [],
+  status: 'loading',
+  statusLevel: '',
+  responseCode: null,
   statusMessage: '',
   action: '',
 };
 
-const noDataMessage = 'No data retrieved for acknowledgement receipts';
-
 export const listAReceipt = createAsyncThunk('listAReceipt', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
-
+  try {
   const response = await axiosInstance.get(`rest/acknowledgement-receipts?token=${accessToken}`);
-
-  if (typeof response !== 'undefined') {
-    const { status } = response;
-    if (status === 200) {
-      if (response.data.length === 0) {
-        response.statusText = `${message.API_200_EMPTY} in acknowledgement receipt.`;
-      } else {
-        response.statusText = `${message.API_200_SUCCESS} in acknowledgement receipt.`;
-      }
-      return response;
+  const { response: validateResponse, valid } = checkResponseValidity(response);
+    if (valid) {
+      return validateResponse;
     }
-
-    if (status === 500 || status === 400) {
-      return thunkAPI.rejectWithValue(response);
-    }
-  } else {
+    return thunkAPI.rejectWithValue(validateResponse);
+  } catch (err) {
     return thunkAPI.rejectWithValue({
-      status: 'error',
+      status: null,
+      data: null,
+      statusText: message.ERROR_OCCURED
     });
   }
-
-  return response;
 });
 
 export const listAReceiptByDepot = createAsyncThunk(
   'listAReceiptByDepot',
   async (payload, thunkAPI) => {
     const accessToken = thunkAPI.getState().auth.token;
+    try {
+      const response = await axiosInstance.get(
+        `rest/acknowledgement-receipts/depot/${payload.depot}?token=${accessToken}`
+      );
 
-    const response = await axiosInstance.get(
-      `rest/acknowledgement-receipts/depot/${payload.depot}?token=${accessToken}`
-    );
-
-    if (typeof response !== 'undefined' && response.status === 200) {
-      const { data } = response;
-      if (data.length === 0) {
-        payload.message.warning(`${noDataMessage} from the selected depot`);
+      const { response: validateResponse, valid } = checkResponseValidity(response);
+      if (valid) {
+        return validateResponse;
       }
-    } else {
-      payload.message.error(message.ITEMS_GET_REJECTED);
-      return thunkAPI.rejectWithValue(response);
+      return thunkAPI.rejectWithValue(validateResponse);
+    } catch (err) {
+      return thunkAPI.rejectWithValue({
+        status: null,
+        data: null,
+        statusText: message.ERROR_OCCURED
+      });
     }
-
-    return response;
   }
 );
 
@@ -67,45 +59,65 @@ export const listAReceiptWithSIByDepot = createAsyncThunk(
   'listAReceiptWithSIByDepot',
   async (payload, thunkAPI) => {
     const accessToken = thunkAPI.getState().auth.token;
-
-    const response = await axiosInstance.get(
-      `rest/acknowledgement-receipts/depot/${payload.depot}/with-si?token=${accessToken}`
-    );
-
-    if (typeof response !== 'undefined' && response.status === 200) {
-      const { data } = response;
-      if (data.length === 0) {
-        payload.message.warning(
-          `${noDataMessage} with an associated sales invoice from the selected depot`
-        );
+    try {
+      const response = await axiosInstance.get(
+        `rest/acknowledgement-receipts/depot/${payload.depot}/with-si?token=${accessToken}`
+      );
+      const { response: validateResponse, valid } = checkResponseValidity(response);
+      if (valid) {
+        return validateResponse;
       }
-    } else {
-      payload.message.error(message.ITEMS_GET_REJECTED);
-      return thunkAPI.rejectWithValue(response);
+      return thunkAPI.rejectWithValue(validateResponse);
+    } catch (err) {
+      return thunkAPI.rejectWithValue({
+        status: null,
+        data: null,
+        statusText: message.ERROR_OCCURED
+      });
     }
-
-    return response;
   }
 );
 
 export const addAReceipt = createAsyncThunk('addAReceipt', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
-
-  const response = await axiosInstance.post(
-    `rest/acknowledgement-receipts/?token=${accessToken}`,
-    payload
-  );
-  return response;
+  try {
+    const response = await axiosInstance.post(
+      `rest/acknowledgement-receipts/?token=${accessToken}`,
+      payload
+    );
+    const { response: validateResponse, valid } = checkResponseValidity(response);
+    if (valid) {
+      return validateResponse;
+    }
+    return thunkAPI.rejectWithValue(validateResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: message.ERROR_OCCURED
+    });
+  }
 });
 
 export const deleteAReceipt = createAsyncThunk('deleteAReceipt', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
-
-  const response = await axiosInstance.post(
-    `rest/acknowledgement-receipts/delete?token=${accessToken}`,
-    payload
-  );
-  return response;
+  try {
+    const response = await axiosInstance.post(
+      `rest/acknowledgement-receipts/delete?token=${accessToken}`,
+      payload
+    );
+    const { response: validateResponse, valid } = checkResponseValidity(response);
+    if (valid) {
+      return validateResponse;
+    }
+    return thunkAPI.rejectWithValue(validateResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: message.ERROR_OCCURED
+    });
+  }
 });
 
 const acknowledgementReceiptSlice = createSlice({
@@ -116,84 +128,174 @@ const acknowledgementReceiptSlice = createSlice({
   },
   extraReducers: {
     [listAReceipt.pending]: (state) => {
-      state.status = 'loading';
+      return {
+        ...state,
+        action: 'fetch',
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for acknowledgement receipts`,
+      };
     },
     [listAReceipt.fulfilled]: (state, action) => {
-      const { data } = action.payload;
-      let statusMessage = message.ITEMS_GET_FULFILLED;
-
-      if (data.length === 0) {
-        statusMessage = noDataMessage;
-      }
+      const { data, status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipts',
+        state.action
+      );
 
       return {
         ...state,
         list: data,
         status: 'succeeded',
-        action: 'get',
+        statusLevel: level,
+        responseCode: status,
         statusMessage,
       };
     },
-    [listAReceipt.rejected]: (state) => {
+    [listAReceipt.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipts',
+        state.action
+      );
+
       return {
         ...state,
+        list: [],
         status: 'failed',
-        action: 'get',
-        statusMessage: message.ITEMS_GET_REJECTED,
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
       };
     },
     [listAReceiptByDepot.pending]: (state) => {
-      state.status = 'loading';
+      return {
+        ...state,
+        action: 'fetch',
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for acknowledgement receipts`,
+      };
     },
     [listAReceiptByDepot.fulfilled]: (state, action) => {
-      const { data } = action.payload;
-      let statusMessage = message.ITEMS_GET_FULFILLED;
-
-      if (data.length === 0) {
-        statusMessage = `${noDataMessage} from the selected depot`;
-      }
+      const { data, status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipts',
+        state.action
+      );
 
       return {
         ...state,
         list: data,
         status: 'succeeded',
-        action: 'get',
+        statusLevel: level,
+        responseCode: status,
         statusMessage,
       };
     },
-    [listAReceiptByDepot.rejected]: (state) => {
+    [listAReceiptByDepot.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipts',
+        state.action
+      );
+
       return {
         ...state,
+        list: [],
         status: 'failed',
-        action: 'get',
-        statusMessage: message.ITEMS_GET_REJECTED,
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
       };
     },
     [listAReceiptWithSIByDepot.pending]: (state) => {
-      state.status = 'loading';
+       return {
+        ...state,
+        action: 'fetch',
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for acknowledgement receipts`,
+      };
     },
     [listAReceiptWithSIByDepot.fulfilled]: (state, action) => {
-      const { data } = action.payload;
-      let statusMessage = message.ITEMS_GET_FULFILLED;
-
-      if (data.length === 0) {
-        statusMessage = `${noDataMessage} with an associated sales invoice from the selected depot`;
-      }
+      const { data, status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipts',
+        state.action
+      );
 
       return {
         ...state,
         list: data,
         status: 'succeeded',
-        action: 'get',
+        statusLevel: level,
+        responseCode: status,
         statusMessage,
       };
     },
-    [listAReceiptWithSIByDepot.rejected]: (state) => {
+    [listAReceiptWithSIByDepot.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipts',
+        state.action
+      );
+
+      return {
+        ...state,
+        list: [],
+        status: 'failed',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
+    },
+    [addAReceipt.pending]: (state) => {
+      return {
+        ...state,
+        action: 'create',
+        status: 'loading',
+        statusMessage: `${message.ITEM_ADD_PENDING} for acknowledgement receipt`,
+        statusLevel: '',
+        responseCode: null,
+      };
+    },
+    [addAReceipt.fulfilled]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipt',
+        state.action
+      );
+
+      return {
+        ...state,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
+    },
+    [addAReceipt.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Acknowledgement Receipt',
+        state.action
+      );
+
       return {
         ...state,
         status: 'failed',
-        action: 'get',
-        statusMessage: message.ITEMS_GET_REJECTED,
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
       };
     },
   },
