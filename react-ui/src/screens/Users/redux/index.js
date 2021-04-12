625d97c6f549de61b65ed01466bb4251981fa940
Angelo Matias <geloko@users.noreply.github.com>
2021-04-12 20:47:20 +0800

    users error handling

--- react-ui/src/screens/Users/redux/index.js
+++ react-ui/src/screens/Users/redux/index.js
@@ -2,58 +2,150 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 import axiosInstance from '../../../utils/axios-instance';
 import * as message from '../../../data/constants/response-message.constant';
+import { checkResponseValidity, generateStatusMessage } from '../../../helpers/general-helper';
 
 const initialState = {
   listUser: null,
   listPermission: null,
+  status: 'loading',
+  statusLevel: '',
+  responseCode: null,
+  statusMessage: '',
+  action: '',
 };
 
-export const listUser = createAsyncThunk('listUser', async (payload, thunkAPI, rejectWithValue) => {
+export const listUser = createAsyncThunk('listUser', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
 
-  const response = await axiosInstance.get(
-    `rest/users/company/${payload.company}/?token=${accessToken}`
-  );
+  try {
+    const response = await axiosInstance.get(
+      `rest/users/company/${payload.company}/?token=${accessToken}`
+    );
 
-  if (typeof response !== 'undefined' && response.status === 200) {
-    const { data } = response;
-    if (data.length === 0) {
-      payload.message.warning('No data retrieved for users');
+    const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+    if (valid) {
+      return validatedResponse;
     }
-  } else {
-    payload.message.error(message.ITEMS_GET_REJECTED);
-    return rejectWithValue(response);
+    return thunkAPI.rejectWithValue(validatedResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
   }
+});
 
-  return response;
+export const listUserByCompanyGroupByDepartment = createAsyncThunk('listUserByCompanyGroupByDepartment', async (payload, thunkAPI) => {
+  const accessToken = thunkAPI.getState().auth.token;
+  const departments = thunkAPI.getState().maintenance.departmentArea.deptList;
+
+  try {
+    const response = await axiosInstance.get(
+      `rest/users/company/${payload.company}/?token=${accessToken}`
+    );
+
+    const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+    if (valid && departments.length > 0) {
+      return validatedResponse;
+    }
+    else if(departments.length === 0 ){
+      validatedResponse.statusText = 'failed. No data retrieved for departments'
+    }
+    return thunkAPI.rejectWithValue(validatedResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
+  }
 });
 
 export const addUser = createAsyncThunk('addUser', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
 
-  const response = await axiosInstance.post(`rest/users/?token=${accessToken}`, payload);
-  return response;
+  try {
+    const response = await axiosInstance.post(`rest/users/?token=${accessToken}`, payload);
+
+    const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+    if (valid) {
+      return validatedResponse;
+    }
+    return thunkAPI.rejectWithValue(validatedResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
+  }
 });
 
 export const updateUser = createAsyncThunk('updateUser', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
 
-  const response = await axiosInstance.post(`rest/users/edit/?token=${accessToken}`, payload);
-  return response;
+  try {
+    const response = await axiosInstance.post(`rest/users/edit/?token=${accessToken}`, payload);
+
+    const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+    if (valid) {
+      return validatedResponse;
+    }
+    return thunkAPI.rejectWithValue(validatedResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
+  }
 });
 
 export const deleteUser = createAsyncThunk('deleteUser', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
 
-  const response = await axiosInstance.post(`rest/users/delete?token=${accessToken}`, payload);
-  return response;
+  try {
+    const response = await axiosInstance.post(`rest/users/delete?token=${accessToken}`, payload);
+
+    const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+    if (valid) {
+      return validatedResponse;
+    }
+    return thunkAPI.rejectWithValue(validatedResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
+  }
 });
 
 export const listPermission = createAsyncThunk('listPermission', async (payload, thunkAPI) => {
   const accessToken = thunkAPI.getState().auth.token;
 
-  const response = await axiosInstance.get(`rest/permissions/?token=${accessToken}`);
-  return response;
+  try {
+    const response = await axiosInstance.get(`rest/permissions/?token=${accessToken}`);
+
+    const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+    if (valid) {
+      return validatedResponse;
+    }
+    return thunkAPI.rejectWithValue(validatedResponse);
+  } catch (err) {
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
+  }
 });
 
 const userSlice = createSlice({
@@ -63,34 +155,193 @@ const userSlice = createSlice({
     clearData: () => initialState,
   },
   extraReducers: {
-    [listUser.pending]: (state, action) => {
-      state.status = 'loading';
+    [listUser.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'fetch', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Users` 
+      };
     },
     [listUser.fulfilled]: (state, action) => {
-      if (action.payload !== undefined && action.payload.status === 200) {
-        state.status = 'succeeded';
-        state.listUser = action.payload.data;
-      } else {
-        state.status = 'failed';
-      }
+      const { data, status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Users', state.action);
+
+      return {
+        ...state,
+        listUser: data,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
     },
     [listUser.rejected]: (state, action) => {
-      state.status = 'failed';
-    },
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Users',
+        state.action
+      );
 
-    [listPermission.pending]: (state, action) => {
-      state.status = 'loading';
+      return {
+        ...state,
+        data: [],
+        status: 'failed',
+        statusLevel: level,
+        responseCode: null,
+        statusMessage,
+      };
+    },
+    [listPermission.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'fetch', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Permissions` 
+      };
     },
     [listPermission.fulfilled]: (state, action) => {
-      if (action.payload !== undefined && action.payload.status === 200) {
-        state.status = 'succeeded';
-        state.listPermission = action.payload.data;
-      } else {
-        state.status = 'failed';
-      }
+      const { data, status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Permissions', state.action);
+
+      return {
+        ...state,
+        listPermission: data,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
     },
     [listPermission.rejected]: (state, action) => {
-      state.status = 'failed';
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Permissions',
+        state.action
+      );
+
+      return {
+        ...state,
+        data: [],
+        status: 'failed',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
+    },
+    [addUser.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'create', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Users` 
+      };
+    },
+    [addUser.fulfilled]: (state, action) => {
+      const { status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Users', state.action);
+
+      return {
+        ...state,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
+    },
+    [addUser.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Users',
+        state.action
+      );
+
+      return {
+        ...state,
+        status: 'failed',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
+    },
+    [updateUser.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'update', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Users` 
+      };
+    },
+    [updateUser.fulfilled]: (state, action) => {
+      const { status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Users', state.action);
+
+      return {
+        ...state,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
+    },
+    [updateUser.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Users',
+        state.action
+      );
+
+      return {
+        ...state,
+        status: 'failed',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
+    },
+    [deleteUser.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'delete', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Users` 
+      };
+    },
+    [deleteUser.fulfilled]: (state, action) => {
+      const { status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Users', state.action);
+
+      return {
+        ...state,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
+    },
+    [deleteUser.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Users',
+        state.action
+      );
+
+      return {
+        ...state,
+        status: 'failed',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
     },
   },
 });
