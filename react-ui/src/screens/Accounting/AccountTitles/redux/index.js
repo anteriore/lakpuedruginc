873b66f79cce4c16a8de0f4fe1ad20cf11cb3b97
Angelo Matias <geloko@users.noreply.github.com>
2021-04-13 19:22:16 +0800

    error handling and cleanup

--- react-ui/src/screens/Accounting/AccountTitles/redux/index.js
+++ react-ui/src/screens/Accounting/AccountTitles/redux/index.js
@@ -6,7 +6,9 @@ import { checkResponseValidity, generateStatusMessage } from '../../../../helper
 
 const initialState = {
   list: [],
-  status: '',
+  status: 'loading',
+  statusLevel: '',
+  responseCode: null,
   statusMessage: '',
   action: '',
 };
@@ -25,7 +27,11 @@ export const listAccountTitles = createAsyncThunk('listAccountTitles', async (pa
     }
     return thunkAPI.rejectWithValue(validatedResponse);
   } catch (err) {
-    return thunkAPI.rejectWithValue(err.response.data);
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
   }
 });
 
@@ -44,20 +50,65 @@ export const listAccountTitlesByType = createAsyncThunk('listAccountTitlesByType
     }
     return thunkAPI.rejectWithValue(validatedResponse);
   } catch (err) {
-    return thunkAPI.rejectWithValue(err.response.data);
+    return thunkAPI.rejectWithValue({
+      status: null,
+      data: null,
+      statusText: 'failed. An error has occurred'
+    });
   }
 });
 
-export const addAccountTitle = createAsyncThunk(
-  'addAccountTitle',
+export const createAccountTitle = createAsyncThunk(
+  'createAccountTitle',
   async (payload, thunkAPI) => {
     const accessToken = thunkAPI.getState().auth.token;
 
-    const response = await axiosInstance.post(
-      `rest/account-titles/?token=${accessToken}`,
-      payload
-    );
-    return response;
+    try {
+      const response = await axiosInstance.post(
+        `rest/account-titles/?token=${accessToken}`,
+        payload
+      );
+
+      const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+      if (valid) {
+        return validatedResponse;
+      }
+      return thunkAPI.rejectWithValue(validatedResponse);
+    } catch (err) {
+      return thunkAPI.rejectWithValue({
+        status: null,
+        data: null,
+        statusText: 'failed. An error has occurred'
+      });
+    }
+  }
+);
+
+export const updateAccountTitle = createAsyncThunk(
+  'updateAccountTitle',
+  async (payload, thunkAPI) => {
+    const accessToken = thunkAPI.getState().auth.token;
+
+    try {
+      const response = await axiosInstance.post(
+        `rest/account-titles/?token=${accessToken}`,
+        payload
+      );
+
+      const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+      if (valid) {
+        return validatedResponse;
+      }
+      return thunkAPI.rejectWithValue(validatedResponse);
+    } catch (err) {
+      return thunkAPI.rejectWithValue({
+        status: null,
+        data: null,
+        statusText: 'failed. An error has occurred'
+      });
+    }
   }
 );
 
@@ -66,11 +117,26 @@ export const deleteAccountTitle = createAsyncThunk(
   async (payload, thunkAPI) => {
     const accessToken = thunkAPI.getState().auth.token;
 
-    const response = await axiosInstance.post(
-      `rest/account-titles/delete?token=${accessToken}`,
-      payload
-    );
-    return response;
+    
+    try {
+      const response = await axiosInstance.post(
+        `rest/account-titles/delete?token=${accessToken}`,
+        payload
+      );
+
+      const { response: validatedResponse, valid } = checkResponseValidity(response);
+
+      if (valid) {
+        return validatedResponse;
+      }
+      return thunkAPI.rejectWithValue(validatedResponse);
+    } catch (err) {
+      return thunkAPI.rejectWithValue({
+        status: null,
+        data: null,
+        statusText: 'failed. An error has occurred'
+      });
+    }
   }
 );
 
@@ -81,50 +147,191 @@ const accountTitleSlice = createSlice({
     clearData: () => initialState,
   },
   extraReducers: {
-    [listAccountTitles.pending]: (state, action) => {
-      state.status = 'loading';
+    [listAccountTitles.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'fetch', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Account Titles` 
+      };
     },
     [listAccountTitles.fulfilled]: (state, action) => {
       const { data, status } = action.payload;
-      const { message, level } = generateStatusMessage(action.payload, 'Account Titles');
+      const { message, level } = generateStatusMessage(action.payload, 'Account Titles', state.action);
 
       return {
         ...state,
         list: data,
-        responseCode: status,
+        status: 'succeeded',
         statusLevel: level,
+        responseCode: status,
         statusMessage: message,
       };
     },
     [listAccountTitles.rejected]: (state, action) => {
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Account Titles',
+        state.action
+      );
+
       return {
         ...state,
+        data: [],
         status: 'failed',
-        action: 'get',
-        statusMessage: message.ITEMS_GET_REJECTED,
+        statusLevel: level,
+        responseCode: null,
+        statusMessage,
       };
     },
-    [listAccountTitlesByType.pending]: (state, action) => {
-      state.status = 'loading';
+    [listAccountTitlesByType.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'fetch', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Account Titles` 
+      };
     },
     [listAccountTitlesByType.fulfilled]: (state, action) => {
       const { data, status } = action.payload;
-      const { message, level } = generateStatusMessage(action.payload, 'Account Titles');
+      const { message, level } = generateStatusMessage(action.payload, 'Account Titles', state.action);
 
       return {
         ...state,
         list: data,
-        responseCode: status,
+        status: 'succeeded',
         statusLevel: level,
+        responseCode: status,
         statusMessage: message,
       };
     },
     [listAccountTitlesByType.rejected]: (state, action) => {
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Account Titles',
+        state.action
+      );
+
+      return {
+        ...state,
+        data: [],
+        status: 'failed',
+        statusLevel: level,
+        responseCode: null,
+        statusMessage,
+      };
+    },
+    [createAccountTitle.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'create', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Account Titles` 
+      };
+    },
+    [createAccountTitle.fulfilled]: (state, action) => {
+      const { status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Account Titles', state.action);
+
+      return {
+        ...state,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
+    },
+    [createAccountTitle.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Account Titles',
+        state.action
+      );
+
       return {
         ...state,
         status: 'failed',
-        action: 'get',
-        statusMessage: message.ITEMS_GET_REJECTED,
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
+    },
+    [updateAccountTitle.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'update', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Account Titles` 
+      };
+    },
+    [updateAccountTitle.fulfilled]: (state, action) => {
+      const { status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Account Titles', state.action);
+
+      return {
+        ...state,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
+    },
+    [updateAccountTitle.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Account Titles',
+        state.action
+      );
+
+      return {
+        ...state,
+        status: 'failed',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
+      };
+    },
+    [deleteAccountTitle.pending]: (state) => {
+      return { 
+        ...state,  
+        action: 'delete', 
+        status: 'loading',
+        statusLevel: '',
+        statusMessage: `${message.ITEMS_GET_PENDING} for Account Titles` 
+      };
+    },
+    [deleteAccountTitle.fulfilled]: (state, action) => {
+      const { status } = action.payload;
+      const { message, level } = generateStatusMessage(action.payload, 'Account Titles', state.action);
+
+      return {
+        ...state,
+        status: 'succeeded',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage: message,
+      };
+    },
+    [deleteAccountTitle.rejected]: (state, action) => {
+      const { status } = action.payload;
+      const { message: statusMessage, level } = generateStatusMessage(
+        action.payload,
+        'Account Titles',
+        state.action
+      );
+
+      return {
+        ...state,
+        status: 'failed',
+        statusLevel: level,
+        responseCode: status,
+        statusMessage,
       };
     },
   },
